36b9b07576342e727729b4e39a7a637811803236
filbertmsaki <msakifil111@gmail.com>
2022-06-11 20:12:39 +0300

    Add Criteria Page

--- resources/views/award/award-criteria.blade.php
+++ resources/views/award/award-criteria.blade.php
@@ -11,7 +11,7 @@
     @endif
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>
-        Awards Category List | Kilimo Marathon, Awards & EXPO
+        Awards Category Criteria | Kilimo Marathon, Awards & EXPO
     </title>
     <link rel="stylesheet" type="text/css" href="{{ asset('asset/css/bootstrap.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('asset/css/slick.css') }}">
